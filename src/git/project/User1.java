ac3f5655edb80011c52ed8d3ab6b4808cb195b1d
Ogai-Ahmadi <106423615+Ogai-Ahmadi@users.noreply.github.com>
2022-08-31 19:49:04 -0700

    1st commit, class user1 is created by user1

--- src/git/project/User1.java
+++ src/git/project/User1.java
@@ -3,6 +3,6 @@ package git.project;
 public class User1 {
     public static void main(String[] args) throws Exception {
         System.out.println("This line is added by user1 class");
-        
+
     }
 }
