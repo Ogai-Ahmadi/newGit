12fcfe58028ff53045924767d36daf1aab3859f1
Ogai-Ahmadi <106423615+Ogai-Ahmadi@users.noreply.github.com>
2022-08-31 19:39:31 -0700

    1st Commit User1 class is added by user1

---
+++ src/git/project/User1.java
@@ -0,0 +1,8 @@
+package git.project;
+
+public class User1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("This line is added by user1 class");
+        
+    }
+}
